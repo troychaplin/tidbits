70518404ef712e9e4777702978e08d49bb6df43b
Troy Chaplin <troy.chaplin@gmail.com>
2026-05-18 21:15:54 -0400

    update dependencies

--- build/blocks/tidbits/index.asset.php
+++ build/blocks/tidbits/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '485dde298ca752e3e165');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '46de682a29503f36d576');
